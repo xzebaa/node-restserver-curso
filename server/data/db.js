c9a05ee43a678ee722153802022997abfe293a9a
Sebastian Alvarez <ext_salvarezl@falabella.cl>
2020-09-01 16:45:16 -0400

    se agrega capa para envio de correo servicio

--- server/data/db.js
+++ server/data/db.js
@@ -1,102 +1,100 @@
-var mysql      = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host     : '149.56.198.135',
-    user     : 'dorrolac_IRON',
-    password : 'hj95hrpkbfej',
-    database : 'dorrolac_IRONHIDE'
+  host: "149.56.198.135",
+  user: "dorrolac_IRON",
+  password: "hj95hrpkbfej",
+  database: "dorrolac_IRONHIDE"
 });
 connection.connect();
 
-
-const login = async (dni) => {
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT * FROM USERS WHERE DNI=${dni}`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
-}
+const login = async dni => {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM USERS WHERE DNI=${dni}`, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        const table = [];
+        resp.forEach(product => {
+          table.push(product);
+        });
+        resolve(table);
+      }
+    });
+  });
+};
 
 const getServices = async () => {
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT * FROM SERVICES`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
-}
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM SERVICES`, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        const table = [];
+        resp.forEach(product => {
+          table.push(product);
+        });
+        resolve(table);
+      }
+    });
+  });
+};
 
 const getReports = async () => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT * FROM REPORTS`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
-}
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM REPORTS`, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        const table = [];
+        resp.forEach(product => {
+          table.push(product);
+        });
+        resolve(table);
+      }
+    });
+  });
+};
 
-const findServiceById = async ( servicesId ) => {
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT * FROM SERVICES where id=${servicesId}`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
-}
+const findServiceById = async servicesId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM SERVICES where id=${servicesId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
+};
 
-const findReportByID = async ( reportId ) => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT * FROM REPORTS WHERE id=${reportId}`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+const findReportByID = async reportId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM REPORTS WHERE id=${reportId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
 };
 
 const createService = async (service = {}) => {
-  return new Promise( (resolve,reject) => {
+  return new Promise((resolve, reject) => {
     var post = {
       date: new Date(),
       activities_services_id: service.activities_services_id,
@@ -106,57 +104,54 @@ const createService = async (service = {}) => {
       categorie_service_id: 100 // por default
     };
 
-    
-    connection.query('INSERT INTO SERVICES SET ?', post, (err, resp) => {
-        if (err) {
-            reject(err)
-        } else {
-            // const table = [];
-            console.log(resp)
-            // resp.forEach((product) => {
-                
-            //     table.push(product)
-            // })
-            resolve(resp)
-        }
+    connection.query("INSERT INTO SERVICES SET ?", post, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        // const table = [];
+        console.log(resp);
+        // resp.forEach((product) => {
+
+        //     table.push(product)
+        // })
+        resolve(resp);
+      }
     });
-  })
-}
+  });
+};
 
 const createReport = async (report = {}) => {
-  return new Promise( (resolve,reject) => {
-      
+  return new Promise((resolve, reject) => {
     var post = {
-        comentary: report.comentary,
-        reported_date: new Date(),
-        informant_dni: report.dni,
-        informant_extra_email: report.extra_email,
-        informant_extra_number1: report.extra_number1,
-        informant_extra_number2: report.extra_number2,
-        active: 1,
-        last_update_date: new Date(),
-        office_id: report.office,
-        status_id: 1,
-        service_id: 100,
-        activities_services_id: report.service
+      comentary: report.comentary,
+      reported_date: new Date(),
+      informant_dni: report.dni,
+      informant_extra_email: report.extra_email,
+      informant_extra_number1: report.extra_number1,
+      informant_extra_number2: report.extra_number2,
+      active: 1,
+      last_update_date: new Date(),
+      office_id: report.office,
+      status_id: 1,
+      service_id: 100,
+      activities_services_id: report.service
     };
 
-      connection.query('INSERT INTO REPORTS SET ?', post, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-            //   console.log(resp)
-              resolve(resp)
-          }
-      })
-  })
-}
+    connection.query("INSERT INTO REPORTS SET ?", post, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        const table = [];
+        //   console.log(resp)
+        resolve(resp);
+      }
+    });
+  });
+};
 
 const insertImageServices = async (service = {}) => {
-  return new Promise( (resolve,reject) => {
-
-    const { service_id, file_name} = service;
+  return new Promise((resolve, reject) => {
+    const { service_id, file_name } = service;
     var post = {
       file_name: file_name,
       date: new Date(),
@@ -164,137 +159,135 @@ const insertImageServices = async (service = {}) => {
       service_id: service_id
     };
 
-    connection.query('INSERT INTO IMAGES_SERVICES SET ?', post, (err, resp) => {
-        if (err) {
-            reject(err)
-        } else {
-            // console.log(resp)
-            resolve(resp);
-        }
+    connection.query("INSERT INTO IMAGES_SERVICES SET ?", post, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        // console.log(resp)
+        resolve(resp);
+      }
     });
   });
 };
 
 const insertImageReport = async (report = {}) => {
-  return new Promise( (resolve,reject) => {
-
-    const { report_id, file_name} = report;
+  return new Promise((resolve, reject) => {
+    const { report_id, file_name } = report;
 
     var post = {
       file_name,
       date: new Date(),
-      active: 1, 
+      active: 1,
       report_id
     };
 
-    connection.query('INSERT INTO IMAGES_REPORTS SET ?', post, (err, resp) => {
-        if (err) {
-            reject(err)
-        } else {
-            // console.log(resp)
-            resolve(resp);
-        }
+    connection.query("INSERT INTO IMAGES_REPORTS SET ?", post, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        // console.log(resp)
+        resolve(resp);
+      }
     });
   });
 };
 
-
 const getActivitiServices = async () => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT id, name FROM ACTIVITIES_SERVICES`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id, name FROM ACTIVITIES_SERVICES`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
 };
 
 const getCategoriesServices = async () => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT id, name FROM CATEGORIES_SERVICES`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id, name FROM CATEGORIES_SERVICES`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
 };
 
 const getOffices = async () => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT id, name FROM OFFICES`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT id, name FROM OFFICES`, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        const table = [];
+        resp.forEach(product => {
+          table.push(product);
+        });
+        resolve(table);
+      }
+    });
+  });
 };
 
-const getCompanys= async () => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT id, name FROM COMPANYS WHERE ACTIVE = 1`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+const getCompanys = async () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id, name FROM COMPANYS WHERE ACTIVE = 1`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
 };
 
-const getOfficesByCompanyId = async ( companyId ) => {
-  
-  return new Promise( (resolve,reject) => {
-      connection.query(`SELECT id, name FROM OFFICES WHERE company_id=${companyId}`, (err, resp) => {
-          if (err) {
-              reject(err)
-          } else {
-              const table = [];
-              resp.forEach((product) => {
-                  
-                  table.push(product)
-              })
-              resolve(table)
-          }
-      })
-  })
+const getOfficesByCompanyId = async companyId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id, name FROM OFFICES WHERE company_id=${companyId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
 };
 
-
-
-const getReportMailForId = async ( reportId ) => {
-  
-    return new Promise( (resolve,reject) => {
-        connection.query(`select RPRT.id ,
+const getReportMailForId = async reportId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `select RPRT.id ,
         USR.name as nombre,
         USR.last_name as apellido,
         USR.dni as rut,
@@ -313,41 +306,99 @@ const getReportMailForId = async ( reportId ) => {
         ON RPRT.informant_dni = USR.dni
         INNER JOIN COMPANYS AS COMPNY
         ON OFFI.company_id = COMPNY.id
-        where RPRT.id=${reportId}`, (err, resp) => {
-            if (err) {
-                reject(err)
-            } else {
-                const table = [];
-                resp.forEach((product) => {
-                    
-                    table.push(product)
-                })
-                resolve(table)
-            }
-        })
-    })
+        where RPRT.id=${reportId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
+};
+
+const getImagesReportByReportId = async reportId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `select id, file_name from IMAGES_REPORTS where report_id=${reportId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
+};
+
+
+const getServiceMailForId = async serviceId => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `select 
+        SERV.id ,
+        SERV.comentary as comentario,
+        USR.name as nombre,
+        USR.last_name as apellido,
+        USR.dni as rut,
+        COMPNY.name as empresa,
+        OFFI.name as oficina,
+        ACTSERVICES.name as servicio_name
+        from SERVICES as SERV
+        INNER JOIN OFFICES AS OFFI
+        ON SERV.office_id = OFFI.id
+        INNER JOIN ACTIVITIES_SERVICES AS ACTSERVICES
+        ON SERV.activities_services_id = ACTSERVICES.id
+        INNER JOIN USERS AS USR
+        ON SERV.informant_dni = USR.dni
+        INNER JOIN COMPANYS AS COMPNY
+        ON OFFI.company_id = COMPNY.id
+        where SERV.id=${serviceId}`,
+        (err, resp) => {
+          if (err) {
+            reject(err);
+          } else {
+            const table = [];
+            resp.forEach(product => {
+              table.push(product);
+            });
+            resolve(table);
+          }
+        }
+      );
+    });
   };
 
-  const getImagesReportByReportId = async ( reportId ) => {
-  
-    return new Promise( (resolve,reject) => {
-        connection.query(`select id, file_name from IMAGES_REPORTS where report_id=${reportId}`, (err, resp) => {
-            if (err) {
-                reject(err)
-            } else {
-                const table = [];
-                resp.forEach((product) => {
-                    
-                    table.push(product)
-                })
-                resolve(table)
-            }
-        })
-    })
-  };
+const getImagesServiceByServiceId = async serviceId => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `select id, file_name from IMAGES_SERVICES where service_id=${serviceId}`,
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const table = [];
+          resp.forEach(product => {
+            table.push(product);
+          });
+          resolve(table);
+        }
+      }
+    );
+  });
+};
 
-
-module.exports = { 
+module.exports = {
   login,
   getServices,
   getReports,
@@ -363,5 +414,7 @@ module.exports = {
   getOfficesByCompanyId,
   getCompanys,
   getReportMailForId,
-  getImagesReportByReportId
+  getImagesReportByReportId,
+  getImagesServiceByServiceId,
+  getServiceMailForId
 };
--- server/routes/mail.js
+++ server/routes/mail.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 
 const { 
     getReportMailForId,
-    getImagesReportByReportId
+    getImagesReportByReportId,
+    getImagesServiceByServiceId,
+    getServiceMailForId
 } = require('../data/db');
 
  app.post('/mail/repor/send', async  (req, resp) =>{
@@ -95,5 +97,94 @@ const {
       }
 
  });
+
+app.post('/mail/service/send', async  (req, resp) =>{
+
+  const { id : idService }= req.body;
+
+  if ( !idService ) {
+      return resp.status(400).json({
+          ok:false, 
+          error: {
+              message:'Error en request.'
+          }
+      });
+  }
+
+  try {
+      const repsonseDB = await getServiceMailForId(idService);
+
+      const transporter =  nodemailer.createTransport({
+          host: "mail.dorrola.com",
+          port: 587,
+          auth: {
+            user: "simplecheck@dorrola.com",
+            pass: "Xebitay123"
+          },
+          tls: {
+              rejectUnauthorized: false
+          }
+        });
+
+      const imagesReport = await getImagesServiceByServiceId(idService);
+      
+      let fileStoreReport = '';
+      let attachment = [];
+      console.log(repsonseDB);
+      
+      imagesReport.forEach( (archivo, index)=> {
+
+          attachment.push({path:  archivo.file_name});
+          fileStoreReport += `<p>IMAGEN = ${archivo.file_name}</p> </br>` ;
+      });
+      console.log('fileStoreReport');
+
+      console.log(fileStoreReport);
+      
+  
+      const mailOptions = {
+        from: "simplecheck@dorrola.com",
+      //   to: "x.zebaa@gmail.com, rodrigogarridov@gmail.com",
+        to: "x.zebaa@gmail.com, ",
+        subject: `[SERVICIO] - EMPRESA: ${repsonseDB[0].empresa} - nuevo reporte de servicio `,
+        text: "SIMPLECHECK",
+        attachments: attachment,
+        html: `
+          <p>ID SERVICIO = ${repsonseDB[0].id}</p> </br>
+          <p>NOMBRE = ${repsonseDB[0].nombre}</p> </br>
+          <p>APELLIDO = ${repsonseDB[0].apellido}</p> </br>
+          <p>RUT = ${repsonseDB[0].rut}</p> </br>
+          <p>EMPRESA = ${repsonseDB[0].empresa}</p> </br>
+          <p>OFICINA = ${repsonseDB[0].oficina}</p> </br>
+          <p>SERVICIO PRESTADO = ${repsonseDB[0].servicio_name}</p> </br>
+          <p>COMENTARIO = ${repsonseDB[0].comentario}</p> </br>
+          <p>IMAGENES ADJUNTAS</p> </br>
+          ${fileStoreReport}
+          `
+      };
+  
+      console.log('enviando  mail')
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+  
+      return resp.json({
+          ok: true,
+        });
+    } catch (error) {
+        console.log(error);
+        return resp.status(400).json({
+          ok:false, 
+          error: {
+              message: error.message
+          }
+      });
+    }
+
+});
 
 module.exports = app;
